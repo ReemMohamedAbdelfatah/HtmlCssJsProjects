ce43d08d16debb5abc3dc0e25fdceb7878a8beb7
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2024-09-05 14:26:11 +0300

    New project

--- AutoTextAnimation/index.js
+++ AutoTextAnimation/index.js
@@ -6,7 +6,9 @@ updateText();
 function updateText() {
   characterIndex++;
   containerEl.innerHTML = `
-  <h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>
+  <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
   `;
 
   if (characterIndex === careers[careerIndex].length) {
