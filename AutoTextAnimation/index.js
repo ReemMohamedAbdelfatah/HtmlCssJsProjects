3764ca0b9e658f1ce1701692ed6223a381e3d954
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2024-09-05 14:24:28 +0300

    complete code

--- AutoTextAnimation/index.js
+++ AutoTextAnimation/index.js
@@ -1,19 +1,20 @@
 const containerEl = document.querySelector(".container");
-const careers = ["Youtuber", "Web Developer" , "Freelncer", "Instructor"];
+const careers = ["Youtuber", "Web Developer", "Freelncer", "Instructor"];
 let careerIndex = 0;
 let characterIndex = 0;
 updateText();
 function updateText() {
   characterIndex++;
   containerEl.innerHTML = `
-  <h1>I am a ${careers[careerIndex].slice(0,characterIndex)}</h1>
+  <h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>
   `;
-  
-  if(characterIndex === careers[careerIndex].length)
-  {
+
+  if (characterIndex === careers[careerIndex].length) {
     careerIndex++;
     characterIndex = 0;
-   
   }
-  setTimeout(updateText,400)
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
 }
